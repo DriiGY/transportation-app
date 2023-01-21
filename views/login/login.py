773890cdbb0e95c2ed5c1d5bd69eebbfc5379c9f
RicardoDuarte <ricardo-mano2@hotmail.com>
2023-01-21 22:11:05 +0000

    updated password field in login screen

--- views/login/login.py
+++ views/login/login.py
@@ -1,8 +1,9 @@
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.relativelayout import MDRelativeLayout
 from kivy.clock import Clock
 from kivymd.app import MDApp
-
+from kivy.properties import StringProperty
 Builder.load_file('views/login/login.kv')
 
 class Login(BoxLayout):
@@ -10,3 +11,11 @@ class Login(BoxLayout):
     def __init__(self, **kw) -> None:
         super().__init__(**kw)
 
+
+
+
+class ClickableTextFieldRound(MDRelativeLayout):
+    text = StringProperty()
+    hint_text = StringProperty()
+    # Here specify the required parameters for MDTextFieldRound:
+    # [...]
