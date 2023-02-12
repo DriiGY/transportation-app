642270a70b4cb58ee45a8218d7bb14595a1f8461
derteck <ricardo-mano2@hotmail.com>
2023-02-12 22:23:18 +0000

    added faq screen

--- views/faq/faq.py
+++ views/faq/faq.py
@@ -2,16 +2,13 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
 
-Builder.load_file('views/settings/settings.kv')
+Builder.load_file('views/faq/faq.kv')
 
 
-class SettingsScreen(BoxLayout):
+class Faq(BoxLayout):
     
     def __init__(self, **kw) -> None:
         super().__init__(**kw)
         self.app = MDApp.get_running_app()
     
-    def go_back(self):
-        self.parent.manager.current = "scrn_home"
-
-
+  
