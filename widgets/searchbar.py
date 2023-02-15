435eea020687168382ed3416ad50692b38263338
derteck <ricardo-mano2@hotmail.com>
2023-02-15 19:35:26 +0000

    faq

---
+++ widgets/searchbar.py
@@ -0,0 +1,112 @@
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import Screen
+
+from kivymd.icon_definitions import md_icons
+from kivymd.app import MDApp
+from kivymd.uix.list import OneLineIconListItem
+
+
+Builder.load_string(
+'''
+#:import images_path kivymd.images_path
+
+
+<CustomOneLineIconListItem>:
+
+    IconLeftWidget:
+        icon: root.icon
+
+
+<PreviousMDIcons>:
+
+    BoxLayout:
+        canvas.before:
+            Color:
+                rgba:[0,0,0,1]
+            Rectangle:
+                pos:self.pos
+                size:self.size
+        orientation: 'vertical'
+        spacing: dp(10)
+        padding: dp(20)
+
+        BoxLayout:
+            canvas.before:
+                Color:
+                    rgba:[1,1,1,1]
+                Rectangle:
+                    pos:self.pos
+                    size:self.size
+            size_hint_y: None
+            height: self.minimum_height
+
+            MDIconButton:
+                icon: 'magnify'
+
+            MDTextField:
+                
+                id: search_field
+                hint_text: 'Search most asked'
+                
+
+    
+ '''
+ )
+# on_text: root.set_list_md_icons(self.text, True)
+# RecycleView:
+#         id: rv
+#         key_viewclass: 'viewclass'
+#         key_size: 'height'
+
+#         RecycleBoxLayout:
+#             padding: dp(10)
+#             default_size: None, dp(48)
+#             default_size_hint: 1, None
+#             size_hint_y: None
+#             height: self.minimum_height
+#             orientation: 'vertical'
+class CustomOneLineIconListItem(OneLineIconListItem):
+     icon = StringProperty()
+
+
+class PreviousMDIcons(Screen):
+    pass
+    # def set_list_md_icons(self, text="", search=False):
+    #     '''Builds a list of icons for the screen MDIcons.'''
+    #     for name_icon in md_icons.keys():
+    #         if search:
+    #             if text in name_icon:
+    #                 self.add_icon_item(name_icon)
+    #         else:
+    #             self.add_icon_item(name_icon)
+
+    # def add_icon_item(self,name_icon):
+    #     self.ids.rv.data.append(
+    #         {
+    #             "viewclass": "CustomOneLineIconListItem",
+    #             "icon": name_icon,
+    #             "text": name_icon,
+    #             "callback": lambda x: x,
+    #         }
+    #     )
+
+    #     self.ids.rv.data = []
+    
+    
+
+
+class MainApp(MDApp):
+  def __init__(self, **kwargs):
+     super().__init__(**kwargs)
+     self.screen = PreviousMDIcons()
+
+  def build(self):
+      return self.screen
+
+  def on_start(self):
+      #self.screen.set_list_md_icons()
+      pass
+
+
+MainApp().run()
