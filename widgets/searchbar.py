e05997770203cc75ef688716dfb94125fd22ceb4
derteck <ricardo-mano2@hotmail.com>
2023-02-16 22:16:07 +0000

    Added couple of questions to FAQS

--- widgets/searchbar.py
+++ widgets/searchbar.py
@@ -5,72 +5,60 @@ from kivy.uix.screenmanager import Screen
 from kivymd.icon_definitions import md_icons
 from kivymd.app import MDApp
 from kivymd.uix.list import OneLineIconListItem
-
+from kivy.uix.boxlayout import BoxLayout
 
 Builder.load_string(
 '''
-#:import images_path kivymd.images_path
+# #:import images_path kivymd.images_path
+
+<SearchBar>:
+    canvas.before:
+        Color:
+            rgba:[0,0,0,0]
+        Rectangle:
+            pos:self.pos
+            size:self.size
+    size_hint_y: None
+    height: self.minimum_height
+    MDIconButton:
+        icon: 'magnify'
+    MDTextField:
+        
+        id: search_field
+        hint_text: 'Search most asked'
 
 
-<CustomOneLineIconListItem>:
-
-    IconLeftWidget:
-        icon: root.icon
-
-
-<PreviousMDIcons>:
-
-    BoxLayout:
-        canvas.before:
-            Color:
-                rgba:[0,0,0,1]
-            Rectangle:
-                pos:self.pos
-                size:self.size
-        orientation: 'vertical'
-        spacing: dp(10)
-        padding: dp(20)
-
-        BoxLayout:
-            canvas.before:
-                Color:
-                    rgba:[1,1,1,1]
-                Rectangle:
-                    pos:self.pos
-                    size:self.size
-            size_hint_y: None
-            height: self.minimum_height
-
-            MDIconButton:
-                icon: 'magnify'
-
-            MDTextField:
-                
-                id: search_field
-                hint_text: 'Search most asked'
-                
-
-    
  '''
  )
+# <SearchBar>:
+
+    
+#     canvas.before:
+#         Color:
+#             rgba:[0,0,0,0]
+#         Rectangle:
+#             pos:self.pos
+#             size:self.size
+#     orientation: 'vertical'
+#     spacing: dp(10)
+#     padding: dp(20)
+
+
 # on_text: root.set_list_md_icons(self.text, True)
 # RecycleView:
-#         id: rv
-#         key_viewclass: 'viewclass'
-#         key_size: 'height'
+#     id: rv
+#     key_viewclass: 'viewclass'
+#     key_size: 'height'
 
-#         RecycleBoxLayout:
-#             padding: dp(10)
-#             default_size: None, dp(48)
-#             default_size_hint: 1, None
-#             size_hint_y: None
-#             height: self.minimum_height
-#             orientation: 'vertical'
-class CustomOneLineIconListItem(OneLineIconListItem):
-     icon = StringProperty()
+#     RecycleBoxLayout:
+#         padding: dp(10)
+#         default_size: None, dp(48)
+#         default_size_hint: 1, None
+#         size_hint_y: None
+#         height: self.minimum_height
+#         orientation: 'vertical'
 
-
-class PreviousMDIcons(Screen):
+class SearchBar(BoxLayout):
     pass
     # def set_list_md_icons(self, text="", search=False):
     #     '''Builds a list of icons for the screen MDIcons.'''
@@ -96,17 +84,17 @@ class PreviousMDIcons(Screen):
     
 
 
-class MainApp(MDApp):
-  def __init__(self, **kwargs):
-     super().__init__(**kwargs)
-     self.screen = PreviousMDIcons()
+# class MainApp(MDApp):
+#   def __init__(self, **kwargs):
+#      super().__init__(**kwargs)
+#      self.screen = PreviousMDIcons()
 
-  def build(self):
-      return self.screen
+#   def build(self):
+#       return self.screen
 
-  def on_start(self):
-      #self.screen.set_list_md_icons()
-      pass
+#   def on_start(self):
+#       #self.screen.set_list_md_icons()
+#       pass
 
 
-MainApp().run()
+# MainApp().run()
