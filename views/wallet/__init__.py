87485bcb32192f15d4b855c23aa87d983aa5f8f6
derteck <ricardo-mano2@hotmail.com>
2023-05-06 21:21:54 +0100

    ...

--- views/wallet/__init__.py
+++ views/wallet/__init__.py
@@ -1,1 +1,2 @@
 from .wallet import Wallet
+from .add_payment import AddPayment
--- views/wallet/wallet.py
+++ views/wallet/wallet.py
@@ -1,10 +1,20 @@
 from kivy.lang import Builder
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.app import MDApp
+from kivy.properties import StringProperty
 Builder.load_file('views/wallet/wallet.kv')
 
 
 class Wallet(MDBoxLayout):
+    def __init__(self, **kw) -> None:
+        super().__init__(**kw)
+        self.app = MDApp.get_running_app()
+
+
+
+class PaymentCardsListItem(MDBoxLayout):
+    card_icon = StringProperty("")
+    card_number = StringProperty("")
     def __init__(self, **kw) -> None:
         super().__init__(**kw)
         self.app = MDApp.get_running_app()
